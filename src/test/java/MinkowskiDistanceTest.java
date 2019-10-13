b3dfefb3db998fefd9e6163ce41df4bc970cc082
Kamil Burda <xburdakamil@gmail.com>
2019-10-13 23:45:07 +0200

    MinkowskiDistance: Add more tests

--- src/test/java/MinkowskiDistanceTest.java
+++ src/test/java/MinkowskiDistanceTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -7,6 +8,7 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class MinkowskiDistanceTest {
@@ -15,12 +17,27 @@ class MinkowskiDistanceTest {
     @MethodSource("compareParams")
     void compare(List<Double> vector1, List<Double> vector2, double power, double expectedDistance) {
         DistanceMetric minkowskiDistance = new MinkowskiDistance(power);
-        assertEquals(expectedDistance, minkowskiDistance.compare(vector1, vector2));
+        assertEquals(expectedDistance, minkowskiDistance.compare(vector1, vector2), 0.01);
     }
 
     static Stream<Arguments> compareParams() {
         return Stream.of(
-            arguments(Arrays.asList(1.0, 3.0), Arrays.asList(4.0, 7.0), 1, 7.0)
+            arguments(Arrays.asList(1.0, 3.0), Arrays.asList(4.0, 7.0), 1, 7.0),
+            arguments(Arrays.asList(1.0, 3.0), Arrays.asList(4.0, 7.0), 2, 5.0)
         );
+    }
+
+    @Test
+    void compare_FirstVectorIsNull_ThrowsException() {
+        assertThrows(NullPointerException.class, () -> {
+            new MinkowskiDistance(1).compare(null, Arrays.asList(4.0, 7.0));
+        });
+    }
+
+    @Test
+    void compare_SecondVectorIsNull_ThrowsException() {
+        assertThrows(NullPointerException.class, () -> {
+            new MinkowskiDistance(1).compare(Arrays.asList(4.0, 7.0), null);
+        });
     }
 }
